2bf4f9f741e867c21c0ccb6ed8771fdd73fe7bad
tuhin902 <tuhin902md@gmail.com>
2022-09-04 16:48:22 +0600

    final commit

--- js/main.js
+++ js/main.js
@@ -33,6 +33,7 @@ const displayNews = async () => {
     // console.log(newsData);
 
     const newsContainer = document.getElementById('news-container');
+    const spinner = document.getElementById('spinner');
 
     newsContainer.textContent = '';
 
@@ -108,7 +109,48 @@ const totalItem = async () => {
 }
 totalItem();
 
-const showCategoryNews = () => {
-
+const showCategoryNews = async () => {
+    const data = await loadAllNews();
+    const showCategory = data.data;
+    // console.log(showCategory);
+    const categoryContainer = document.getElementById('category-container');
+    categoryContainer.textContent = '';
+    // console.log(categoryContainer);
+    showCategory.forEach(show => {
+        const { author, details, thumbnail_url, title, total_view } = show;
+        console.log(show);
+        const div = document.createElement('div');
+        div.innerHTML = `<div class="card mb-3  p-3">
+        <div class="row g-0 conatiner-fluid">
+            <div class="col-md-4">
+                <img src=${thumbnail_url} class="img-fluid rounded-start" alt="...">
+            </div>
+            <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">${title}</h5>
+                    <p class="card-text">${details.length > 200 ? details.slice(0, 200) + '...' : details}</p>
+                </div>
+                    <div class="card-body d-flex justify-content-between">
+                        <div class="d-flex ">
+                        <a class="navbar-brand m-3  " href="#">
+                        <img src=${author.img} alt="" width="30" height="30">
+                       </a>          
+                        <div>
+                        <h5>${author.name === "system" ? 'no data found' : author.name}</h5>
+                        <p>${author.published_date}</p>
+                        </div>
+                    </div>
+                    <p class="my-auto"> <i class="fa-regular fa-eye"></i> ${total_view === 0 ? 'no data found' : total_view}</p>
+    
+                    <button type="button" onclick="showModal('${title}','${thumbnail_url}')" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
+                    show details
+                    </button>
+    
+                </div>
+            </div>
+        </div>
+    </div>`
+        categoryContainer.appendChild(div);
+    })
 }
-showCategoryNews();
+// showCategoryNews();
